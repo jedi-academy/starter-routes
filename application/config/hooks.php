810b20c0ce1e5ae61ca26b0224fd1999f0c2e32f
peymantp <peymantparsa@gmail.com>
2016-10-02 13:28:24 -0700

    working on hooks

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+$hook['display_override'] = array(
+    'class'    => 'MyHook',
+    'function' => 'censor_words',
+    'filename' => 'MyHook.php',
+    'filepath' => 'hooks',
+    'params'   => array('controller')
+    );
