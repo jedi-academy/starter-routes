4ce7051c6011131fb2118124f19c0793b28b4e07
Karan Parmar <karandipparmar@hotmail.com>
2017-01-31 19:07:15 -0800

    Closes #7 - Fixed issue 07 (bingo)

--- application/controllers/Bingo.php
+++ application/controllers/Bingo.php
@@ -1,24 +1,24 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
-class bingo extends Application
-{
-	function __construct()
-	{
-		parent::__construct();
-	}
-	/**
-	 * Homepage for our app
-	 */
-	public function index()
-	{
-		// The view we want shown
-		$this->data['pagebody'] = 'justone';
-		
-		// Building list of authors, to pass on to our view 
-		$source = $this->quotes->get(5);
-		
-		$this->data = array_merge($this->data, $source);
-				
-		$this->render();
-	}
+
+class Bingo extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        // loads justone
+        $this->data['pagebody'] = 'justone';
+
+        // gets the quote
+        $source = $this->quotes->get(5);
+
+        // merge the records to data array
+        $this->data = array_merge($this->data, $source);
+
+        $this->render();
+    }
+
 }
