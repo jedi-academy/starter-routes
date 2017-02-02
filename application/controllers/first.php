ad4c3b79a2d6fc2174a28b57431cbd68480e27d3
ljf920727 <ljf920727@gmail.com>
2017-02-02 11:01:08 -0800

    #3 fixed add the zzz() inside the first controller Signed-off-by:ljf920727 <ljf920727@gmail.com>

--- application/controllers/first.php
+++ application/controllers/first.php
@@ -27,4 +27,16 @@
 
 		$this->render();
      }
+
+    public function zzz(){
+        // this is the view we want shown
+        $this->data['pagebody'] = 'justone';
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get(1);
+        $this->data['mug'] = $source['mug'];
+        $this->data['who'] = $source['who'];
+        $this->data['what'] = $source['what'];
+        $this->render();
+    }
+     
 }
